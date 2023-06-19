a581bb026bcbdeb1d6e6fa09dcd7590c410ea54e
sainaveenbysani <135251127+sainaveenbysani@users.noreply.github.com>
2023-06-19 10:49:46 -0400

    Update validate-input.js

--- src/validate-input.js
+++ src/validate-input.js
@@ -1,40 +1,17 @@
 const core = require('@actions/core');
 
 try {
-  const issueTitle = process.env.GITHUB_EVENT_ISSUE_TITLE;
-  const issueBody = process.env.GITHUB_EVENT_ISSUE_BODY;
-  const issueNumber = process.env.GITHUB_EVENT_ISSUE_NUMBER;
-//  const issueAssignee = process.env.GITHUB_EVENT_ISSUE_ASSIGNEE;
+  const inputData = JSON.parse(core.getInput('data'));
+  const studentName = inputData.StudentName;
+  const rollNo = inputData.RollNo;
+  const idList = inputData.IdList;
 
-//   // Validate inputs
-// //   if (!issueTitle || issueTitle.trim() === '') {
-// //     throw new Error('Issue title is required.');
-// //   }
+  console.log('Student Name:', studentName);
+  console.log('Roll No:', rollNo);
+  console.log('ID List:', idList);
 
-//   if (!issueBody || issueBody.trim() === '') {
-//     throw new Error('Issue description is required.');
-//   }
-
-// //   if (issueLabels.length === 0) {
-// //     throw new Error('At least one label must be assigned to the issue.');
-// //   }
-
-
-  // Print validated inputs
-  console.log(`Issue Title: ${issueTitle}`);
-  console.log(`Issue Description: ${issueBody}`);
-  console.log(`Issue Description: ${issueNumber}`);
- // console.log(`Issue Labels: ${issueLabels.join(', ')}`);
-  //console.log(`Issue Assignees: ${issueAssignees.join(', ')}`);
-
-  // Additional validation or processing logic can be added here
-
-//   // Set outputs for use in subsequent steps
-//   //core.setOutput('validatedTitle', issueTitle);
-//   core.setOutput('validatedDescription', issueBody);
-//   //core.setOutput('validatedLabels', issueLabels.join(','));
-//  // core.setOutput('validatedAssignees', issueAssignees.join(','));
-
+  // Use the extracted values to perform your desired actions
+  // ...
 } catch (error) {
   core.setFailed(error.message);
 }
